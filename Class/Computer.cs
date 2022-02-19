b1858ff56f65814c0078e172ed57927e2101e89e
sena bakacak <senabakacak2@hotmail.com>
2022-02-19 18:02:55 +0300

    oop class

---
+++ Class/Computer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+     class Computer
+    {
+         //Bilgisayar nesnesi yaratmak istediğimiz için computer adında class oluşturduk.
+        
+            string CPU;
+            string DisplayCard;
+            public int MemoryCapacity;
+            public string MemoryType;
+            public bool DisplayScreen;
+            //form1'de özelliklere erişmek için public olarak işaretledik
+        
+    }
+}
---
+++ Class/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Computer monster = new Computer();
+            monster.DisplayScreen = true;
+            monster.MemoryCapacity = 64;
+            monster.MemoryType = "DDR4";
+
+            MessageBox.Show($"Ekran:{monster.DisplayScreen}\nRAM Kapasitesi:{monster.MemoryCapacity}\nRAM türü:{monster.MemoryType}");
+
+        }
+    }
+}
